db10c28ac631c703f872ddcc3aa0fe0c41cd6396
saroayshehzad <saroaynisha@gmail.com>
2026-03-11 03:37:00 +0530

    Add initial settings for govt_scraper project

---
+++ govt_scraper/settings.py
@@ -0,0 +1,7 @@
+BOT_NAME = 'govt_scraper'
+SPIDER_MODULES = ['govt_scraper.spiders']
+NEWSPIDER_MODULE = 'govt_scraper.spiders'
+
+# Helps avoid getting blocked
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+ROBOTSTXT_OBEY = True
