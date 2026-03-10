4433575afc7f3686c47e87908e011a3e2587a39d
saroayshehzad <saroaynisha@gmail.com>
2026-03-11 04:06:05 +0530

    Update USER_AGENT to include Chrome version

--- govt_scraper/settings.py
+++ govt_scraper/settings.py
@@ -3,5 +3,5 @@ SPIDER_MODULES = ['govt_scraper.spiders']
 NEWSPIDER_MODULE = 'govt_scraper.spiders'
 
 # Helps avoid getting blocked
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 ROBOTSTXT_OBEY = True
